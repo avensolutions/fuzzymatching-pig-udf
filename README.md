4cfb85719405ae8d29a3226ad5162884ebff15d3
Jeffrey Aven <info@avensolutions.com>
2015-03-13 21:02:29 +1100

    .gitignore added

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Apache Pig UDF to return n-grams and q-grams used for approximate string matching
 
-## Usage
+## Pig Usage
 
 	REGISTER 'fuzzymatchingudf.py' USING jython as fuzzymatchingudf;
 	raw_data = LOAD 'test.data' USING PigStorage();
@@ -11,3 +11,12 @@ Apache Pig UDF to return n-grams and q-grams used for approximate string matchin
 	
 	names_with_qgrams = FOREACH raw_data GENERATE $0, fuzzymatchingudf.return_qgrams(fuzzymatchingudf.return_ngrams($0,3));
 	DUMP names_with_qgrams;
+	
+	flattened_qgrams = FOREACH names_with_qgrams GENERATE $0, FLATTEN($1);
+	DUMP flattened_qgrams;
+
+## Python Test Usage
+
+	python fuzzymatching-test.py
+	
+![fuzzymatching-udf-python-test-program screenshot](http://avensolutions.com/images/fuzzymatching-udf-python-test-program.png)	
--- fuzzymatching-test.py
+++ fuzzymatching-test.py
@@ -1,20 +1,26 @@
 import os
 
 # prepare temp udf module without outputSchema decorators
-tmpfile = "fuzzymatchingudftmp.py"
+tmpfile = 'fuzzymatchingudftmp.py'
 if os.path.exists(tmpfile):
 	os.remove(tmpfile)
 f = open(tmpfile, 'w')
 file = open('fuzzymatchingudf.py', 'r')
 for line in file:
 	line_str = str(line)
-	if not line_str.startswith("@"):
+	if not line_str.startswith('@'):
 		f.write(line)
 f.close()
 
 import fuzzymatchingudftmp
-print "Enter a name:",
-name = raw_input()
-print "Name entered: " + name
-outbag = fuzzymatchingudftmp.return_ngrams(name,3)
-print outbag
+print "Enter ngram size:",
+ngram_size_str = raw_input()
+ngram_size = int(ngram_size_str)
+test_data = open('test.data', 'r')
+for name in test_data:
+	print 'Input Record : ' + name
+	ngrams = fuzzymatchingudftmp.return_ngrams(name,ngram_size)
+	print 'ngrams : ' + str(ngrams)
+	qgrams = fuzzymatchingudftmp.return_qgrams(ngrams)
+	print 'qgrams : ' + str(qgrams)
+	print '--------'
